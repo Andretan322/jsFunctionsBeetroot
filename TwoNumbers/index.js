b73494e561f65b4e0c22e128638a0c64d01730d7
Andretan322 <andretan322@gmail.com>
2021-03-23 17:39:13 +0200

    create getNumber task using the Arrows Function

--- TwoNumbers/index.js
+++ TwoNumbers/index.js
@@ -25,3 +25,16 @@ let getNumberSecond = function () {
         alert(-1);
     }
 }
+
+let getNumberThird = () => {
+    let askNumberOne = +prompt("Type the first number");
+    let askNumberTwo = +prompt("Type the second number");
+
+    if (askNumberOne > askNumberTwo) {
+        alert(1);
+    } else if (askNumberOne === askNumberTwo) {
+        alert(0);
+    } else {
+        alert(-1);
+    }
+}
