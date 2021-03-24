612c4c6abb1a6a4d177ce949897679557037fecc
Andrey Bandura <andretan322@gmail.com>
2021-03-24 14:55:23 +0200

    create Thre Digits task

---
+++ ThreeDigits/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function askNumbers() {
+
+    let arrNumbers = [];
+
+    for (let i = 1; i <= 3; i++) {
+        const askNumber = prompt("Type the number");
+        arrNumbers.push[askNumber];
+    }
+
+    return arrNumbers;
+}
+
+function getNumber() {
+    askNumbers();
+    alert(arrNumbers.reduce((a, b) => a + b;)
+}
