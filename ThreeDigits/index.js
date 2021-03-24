d965190ce8cb14adb9de2eca01913a874df5b2d5
Andrey Bandura <andretan322@gmail.com>
2021-03-24 15:10:15 +0200

    add else in function askNumbers to stop add empty elements in askNumbers array

--- ThreeDigits/index.js
+++ ThreeDigits/index.js
@@ -6,13 +6,15 @@ function askNumbers() {
 
     for (let i = 1; i <= 3; i++) {
         const askNumber = prompt("Type the number");
-        arrNumbers.push[askNumber];
+        if (askNumber != "") {
+            arrNumbers.push(askNumber);
+        } else continue;
     }
 
     return arrNumbers;
 }
 
 function getNumber() {
-    askNumbers();
-    alert(arrNumbers.reduce((a, b) => a + b));
+    const arr = askNumbers();
+    alert(arr.reduce((a, b) => a + b));
 }
